c1188c8674351cc2e036e30a689129b232eb375d
FadhilAF <falfatih3421@gmail.com>
2022-05-22 16:28:16 +0700

    add register page

--- Client/src/Pages/Register/Register.jsx
+++ Client/src/Pages/Register/Register.jsx
@@ -1,7 +1,82 @@
 import React from "react";
+import styles from "./Register.module.css";
 
 const Register = () => {
-	return <div>Register</div>;
+	return (
+		<div className= { styles.body } >
+			<div className={ styles.container }>
+				<div className={ styles.forms }>
+					<div className={ `${ styles.form } ${  styles.login }` }>
+						<span className={ styles.title }>Registrasi</span>
+
+						<form action="#">
+							<div className={ styles[ "input-field" ] }>
+								<input 
+									type="text" 
+									placeholder="Masukkan Nama" 
+									required
+								/>
+								<i className={ `${ styles.uil } ${ styles[ "uil-user" ] }` }></i> 
+							</div>
+
+							<div className= { styles[  "input-field" ] } >
+								<input 
+									type="text" 
+									placeholder="Masukkan 
+									NIM" 
+									required
+								/>
+								<i className={ `${ styles.uil } ${ styles[ "uil-envelope-shield" ] } ${ styles.icon }` }></i> 
+							</div>
+
+							<div className= { styles[ "input-field" ] } >
+								<input 
+									type="password" 
+									placeholder="Masukkan Password" 
+									required
+								/>
+								<i className={ `${ styles.uil } ${ styles[ "uil-lock-alt" ] }` }></i>
+							</div>
+
+							<div className= { styles[ "input-field" ] } >
+								<input 
+									type="password" 
+									placeholder="Konfirmasi Password" 
+									required
+								/>
+								<i className={ `${ styles.uil } ${ styles[ "uil-lock" ] } ${ styles.icon }` }></i>
+							</div>
+
+							<div className={ styles[ "checkbox-text" ] }>
+								<div className={ styles[ "checkbox-content" ] }>
+									<input 
+										type="checkbox" 
+										id="logCheck" 
+										required
+									/>
+									<label for="logCheck" className={ styles.text }>Accept privacy & policy</label>
+								</div>
+							</div>
+
+							<div className={ ` ${ styles[ "input-field" ] } ${ styles.button } ` }>
+								<input 
+									type="button" 
+									value="Registrasi"
+								/>
+							</div>
+						</form>
+
+						<div className={ styles[ "login-signup" ] }>
+							<span className={ styles.text }>Sudah daftar?
+								<a href="login.html" className={ ` ${ styles.text } ${ styles[ "signup-text" ] } ` }>Login sekarang</a>
+							</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	);
 };
 
 export default Register;
