016e9bfa9495a7769d7c3dd9ef08df7c44216a94
Azie Melza Pratama <aziemp66@gmail.com>
2022-05-22 12:57:07 +0700

    Adding Register Page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,6 +7,7 @@ import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
 import Profile from "./Pages/Profile/Profile";
+import Register from "./Pages/Register/Register";
 
 function App() {
 	return (
@@ -16,6 +17,7 @@ function App() {
 				<Route path="/" element={<Home />} />
 				<Route path="/profile" element={<Profile />} />
 				<Route path="/login" element={<Login />} />
+				<Route path="/register" element={<Register/>}
 			</Routes>
 		</div>
 	);
