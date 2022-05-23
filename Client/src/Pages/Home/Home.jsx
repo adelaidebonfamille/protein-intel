86ae6c8a78f3456ac8a0ce8f1a1b7803feb30786
Azie Melza Pratama <aziemp66@gmail.com>
2022-05-24 00:49:49 +0700

    Integrating Home Css

--- Client/src/Pages/Home/Home.jsx
+++ Client/src/Pages/Home/Home.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import styles from "./Home.module.css";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={styles.about}>
+      <div className={styles["inner-section"]}>
+        <h1>Welcome To Protein 2022</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+          velit ducimus, enim inventore earum, eligendi nostrum pariatur
+          necessitatibus eius dicta a voluptates sit deleniti autem error eos
+          totam nisi neque voluptates sit deleniti autem error eos totam nisi
+          neque.
+        </p>
+        <div className={styles.skills}>
+          <button>Register</button>
+          <button>Sign In</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
