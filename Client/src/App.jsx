402156fb4c0856997ecd100535e3ae65a27ca3d5
FadhilAF <falfatih3421@gmail.com>
2022-05-24 22:35:06 +0700

    add admin page structure

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -8,6 +8,9 @@ import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
 import Profile from "./Pages/Profile/Profile";
 import Register from "./Pages/Register/Register";
+import Admin from "./Pages/Admin/Admin";
+import Problems from "./Pages/Admin/Problems/Problems";
+import Scores from "./Pages/Admin/Scores/Scores";
 
 function App() {
   return (
@@ -18,6 +21,9 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/admin/problems" element={<Problems />} />
+        <Route path="/admin/scores" element={<Scores />} />
       </Routes>
     </div>
   );
---
+++ Client/src/Pages/Admin/Scores/Scores.jsx
@@ -0,0 +1,11 @@
+import styles from './Scores.module.css';
+import axios from "axios";
+
+const Scores = () => {
+    return (
+        <div className={styles.container}>
+        </div>
+    );
+}
+
+export default Scores;
