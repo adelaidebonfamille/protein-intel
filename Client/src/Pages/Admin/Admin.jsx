c48773c2afccf31a90eb7afbfdebc734f24cf88c
FadhilAF <falfatih3421@gmail.com>
2022-05-24 22:37:51 +0700

    vscode error

--- Client/src/Pages/Admin/Admin.jsx
+++ Client/src/Pages/Admin/Admin.jsx
@@ -1,56 +1,54 @@
-import { Link } from 'react-router-dom';
-import styles from './Admin.module.css';
+import { Link } from "react-router-dom";
+import styles from "./Admin.module.css";
 
 const Admin = () => {
-    const onSubmitHandler = async (e) => {
-        e.preventDefault();
-    }
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+  };
 
-    return (
-        <>
-            <div className={styles[ "login-container" ]}>
-                <h1>admin login</h1>
-                <form action="#" onSubmit={onSubmitHandler}>
-                    <div classname={styles["input-field"]}>
-                        <p>email</p>
-                        <input
-                        type="text"
-                        placeholder="masukkan email"
-                        name="email"
-                        required
-                        />
-                    </div>
-                    <div classname={styles["input-field"]}>
-                        <p>password</p>
-                        <input
-                        type="password"
-                        placeholder="masukkan password"
-                        name="password"
-                        required
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        className={`${styles["input-field"]} ${styles.button}`}
-                    >
-                        Login
-                    </button>
-                </form>
-            </div>
-            <div className={styles.container}>
-                <Link to='problems'>            
-                    <div className={styles[ "category-button" ]}>
-                        Change / Delete Uploaded Problems
-                    </div>
-                </Link>
-                <Link to='scores'>
-                    <div className={styles[ "category-button" ]}>
-                        See All User Score
-                    </div>
-                </Link>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className={styles["login-container"]}>
+        <h1>admin login</h1>
+        <form action="#" onSubmit={onSubmitHandler}>
+          <div classname={styles["input-field"]}>
+            <p>email</p>
+            <input
+              type="text"
+              placeholder="masukkan email"
+              name="email"
+              required
+            />
+          </div>
+          <div classname={styles["input-field"]}>
+            <p>password</p>
+            <input
+              type="password"
+              placeholder="masukkan password"
+              name="password"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className={`${styles["input-field"]} ${styles.button}`}
+          >
+            Login
+          </button>
+        </form>
+      </div>
+      <div className={styles.container}>
+        <Link to="problems">
+          <div className={styles["category-button"]}>
+            Change / Delete Uploaded Problems
+          </div>
+        </Link>
+        <Link to="scores">
+          <div className={styles["category-button"]}>See All User Score</div>
+        </Link>
+      </div>
+    </>
+  );
+};
 
 export default Admin;
--- Client/src/Pages/Admin/Scores/Scores.jsx
+++ Client/src/Pages/Admin/Scores/Scores.jsx
@@ -1,11 +1,8 @@
-import styles from './Scores.module.css';
+import styles from "./Scores.module.css";
 import axios from "axios";
 
 const Scores = () => {
-    return (
-        <div className={styles.container}>
-        </div>
-    );
-}
+  return <div className={styles.container}></div>;
+};
 
 export default Scores;
