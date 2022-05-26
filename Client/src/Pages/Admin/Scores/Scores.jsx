805770b05cedba27bf7103757ace22fd7dccde76
Azie Melza Pratama <aziemp66@gmail.com>
2022-05-26 23:42:12 +0700

    Merging Locals

--- Client/src/Pages/Admin/Scores/Scores.jsx
+++ Client/src/Pages/Admin/Scores/Scores.jsx
@@ -2,11 +2,13 @@ import styles from "./Scores.module.css";
 import { Link } from "react-router-dom";
 
 const Scores = () => {
-  return <div className={styles.container}>
-    <Link to="/admin">
+  return (
+    <div className={styles.container}>
+      <Link to="/admin">
         <div className={styles["go-back-home"]}>Go back to Admin Homepage</div>
-    </Link>
-  </div>;
+      </Link>
+    </div>
+  );
 };
 
 export default Scores;
