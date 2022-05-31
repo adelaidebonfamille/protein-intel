9807c7f56fb4d80521e9191deab7e83f02fac744
Azie Melza Pratama <aziemp66@gmail.com>
2022-05-31 20:34:48 +0700

    Completed Forgot Password

--- Client/src/Pages/ForgotPassword/ForgotPassword.jsx
+++ Client/src/Pages/ForgotPassword/ForgotPassword.jsx
@@ -1,10 +1,29 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 import styles from "./ForgotPassword.module.css";
 
+const API_URL =
+  (import.meta.env.API_URL &&
+    `${import.meta.env.API_URL}/api/auth/forgot-password`) ||
+  "http://localhost:5000/api/auth/forgot-password";
+
 const ForgotPassword = () => {
+  const [isMessage, setIsMessage] = useState(null);
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    const response = await axios.post(API_URL, {
+      email: e.target.email.value,
+    });
+    if (response.data.error) {
+      setIsMessage(response.data.error);
+    } else {
+      setIsMessage(response.data.message);
+    }
+  };
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onSubmit={onSubmitHandler}>
       <form className={styles.card}>
         <h2>Enter account email to reset</h2>
 
@@ -16,6 +35,8 @@ const ForgotPassword = () => {
         <button className={styles.button} type="submit">
           Submit
         </button>
+
+        <div className={styles.message}>{isMessage && <p>{isMessage}</p>}</div>
       </form>
     </div>
   );
