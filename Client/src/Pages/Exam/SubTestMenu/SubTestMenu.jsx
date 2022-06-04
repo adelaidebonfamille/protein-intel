a3a3fbdafb3c03f6d86d16c6c5f9a5c921fdf109
Azie Melza Pratama <aziemp66@gmail.com>
2022-06-04 21:27:20 +0700

    Fixing Broken API Routes

--- Client/src/Pages/Exam/SubTestMenu/SubTestMenu.jsx
+++ Client/src/Pages/Exam/SubTestMenu/SubTestMenu.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 const OngoingExam = (props) => {
   const baseUrl =
     (import.meta.env.VITE_API_URL &&
-      `${import.meta.env.VITE_API_URL}/subtest`) ||
+      `${import.meta.env.VITE_API_URL}/test/subtest`) ||
     "http://localhost:5000/api/test/subtest";
 
   const navigate = useNavigate();
