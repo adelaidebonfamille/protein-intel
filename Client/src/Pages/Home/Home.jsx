01e56995ed958f75a2cd024e31f9c59d5e9ee4ff
Azie Melza Pratama <aziemp66@gmail.com>
2022-06-04 11:57:57 +0700

    Replacing Description in Home

--- Client/src/Pages/Home/Home.jsx
+++ Client/src/Pages/Home/Home.jsx
@@ -8,11 +8,11 @@ const Home = () => {
       <div className={styles["inner-section"]}>
         <h1>Welcome To Protein 2022</h1>
         <p className={styles.text}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
-          velit ducimus, enim inventore earum, eligendi nostrum pariatur
-          necessitatibus eius dicta a voluptates sit deleniti autem error eos
-          totam nisi neque voluptates sit deleniti autem error eos totam nisi
-          neque.
+          Proteins is TOEFL/SULIET test simulation for Sriwijaya Univesity
+          Students, where students can practice solving TOEFL like problems.
+          This activity is to introduce Unsri students, especially at the
+          Faculty of Computer Science with SULIET test problems and also help
+          students to improve their ability to answer questions.
         </p>
         <div className={styles.skills}>
           <Link to="/register">
