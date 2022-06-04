36bc71f71c64a66e7b662adf99b22200d61d34c0
Azie Melza Pratama <aziemp66@gmail.com>
2022-06-04 23:45:53 +0700

    Trying to Reset Password through outlook

---
+++ Server/utility/node-mailer.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const nodemailer = require("nodemailer");
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+	service: "hotmail",
+	secure: false, // true for 465, false for other ports
+	auth: {
+		user: process.env.EMAIL_USER, // generated email address
+		pass: process.env.EMAIL_PASS, // generated password
+	},
+});
+
+// setup email data with unicode symbols
+const sendMail = async (content) => {
+	await transporter.sendMail({
+		from: process.env.EMAIL_USER,
+		...content,
+	});
+};
+
+module.exports = sendMail;
