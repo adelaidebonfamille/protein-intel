a7f1e6c410b2865eaac9a2f8fde935063b311229
Azie Melza Pratama <aziemp66@gmail.com>
2022-06-05 17:50:48 +0700

    Deleting Previous Options

--- Server/app.js
+++ Server/app.js
@@ -22,10 +22,6 @@ dotenv.config();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-});
-
 app.use("/problems/files", express.static("./problem-data/files"));
 
 app.use("/api/auth", authRoutes);
