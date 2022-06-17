fe4bde4235aaf65e68427495eda91108e3e192d9
aziemp66 <aziemp66@gmail.com>
2022-06-17 19:54:00 +0700

    Adding Port to nodemailer

--- Server/utility/node-mailer.js
+++ Server/utility/node-mailer.js
@@ -5,6 +5,7 @@ const nodemailer = require("nodemailer");
 let transporter = nodemailer.createTransport({
   service: "hotmail",
   name: "smtp.office365.com",
+  port: 587,
   secure: false, // true for 465, false for other ports
   auth: {
     user: process.env.EMAIL_USER, // generated email address
