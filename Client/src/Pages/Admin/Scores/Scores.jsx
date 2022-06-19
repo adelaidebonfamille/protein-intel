51b501ebece263add84baab8fe7b7908a41c650a
FadhilAF <falfatih3421@gmail.com>
2022-06-19 19:32:47 +0700

    fix error

--- Client/src/Pages/Admin/Scores/Scores.jsx
+++ Client/src/Pages/Admin/Scores/Scores.jsx
@@ -79,7 +79,7 @@ const Scores = () => {
         {selectedScores?.map((score, index) => {
           return (
             <div className={styles.score} key={index}>
-              <h5>Rank {key+1}</h5>
+              <h5>Rank {index+1}</h5>
               <tr>
                 <th>NIM</th>
                 <td>{score.nim}</td>
