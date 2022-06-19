5cb7086e42e3cf2f537e3f03b75f7a972f20a3cb
FadhilAF <falfatih3421@gmail.com>
2022-06-19 19:29:34 +0700

    add rank to show all score

--- Client/src/Pages/Admin/Scores/Scores.jsx
+++ Client/src/Pages/Admin/Scores/Scores.jsx
@@ -79,6 +79,7 @@ const Scores = () => {
         {selectedScores?.map((score, index) => {
           return (
             <div className={styles.score} key={index}>
+              <h5>Rank {key+1}</h5>
               <tr>
                 <th>NIM</th>
                 <td>{score.nim}</td>
