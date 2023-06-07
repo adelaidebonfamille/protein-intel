3811e75c773e650f6a3f49c5df5eab591e706cea
aziemp66 <aziemp66@gmail.com>
2023-06-07 08:42:33 +0700

    Adding Test Routes

--- Server/app.js
+++ Server/app.js
@@ -16,40 +16,44 @@ const userRoutes = require("./routers/user.routes");
 const app = express();
 
 try {
-	app.use(cors());
+  app.use(cors());
 
-	dotenv.config();
+  dotenv.config();
 
-	app.use(bodyParser.json());
-	app.use(bodyParser.urlencoded({ extended: true, limit: "100mb" }));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true, limit: "100mb" }));
 
-	app.use("/problems/files", express.static("./problem-data/files"));
+  app.use("/problems/files", express.static("./problem-data/files"));
 
-	app.use("/api/auth", authRoutes);
+  app.get("/api", (req, res) => {
+    res.send("Hello World!");
+  });
 
-	app.use(verifyTokenMiddleware);
+  app.use("/api/auth", authRoutes);
 
-	app.use("/api/admin", verifyRolesMiddleware.bind(null, "admin"), adminRoutes);
+  app.use(verifyTokenMiddleware);
 
-	app.use("/api/user", verifyRolesMiddleware.bind(null, "user"), userRoutes);
+  app.use("/api/admin", verifyRolesMiddleware.bind(null, "admin"), adminRoutes);
 
-	app.use("/api/test", verifyRolesMiddleware.bind(null, "user"), testRoutes);
+  app.use("/api/user", verifyRolesMiddleware.bind(null, "user"), userRoutes);
 
-	app.use(errorHandlerMiddleware);
+  app.use("/api/test", verifyRolesMiddleware.bind(null, "user"), testRoutes);
+
+  app.use(errorHandlerMiddleware);
 } catch (error) {
-	console.log(error);
+  console.log(error);
 }
 
 const PORT = process.env.PORT || 5000;
 
 db.connectToDatabase()
-	.then(() => {
-		const server = app.listen(PORT, () => {
-			console.log(`Server running on port ${PORT}`);
-		});
-		server.keepAliveTimeout = 61 * 1000;
-		server.headersTimeout = 122 * 1000;
-	})
-	.catch((err) => {
-		console.log(err);
-	});
+  .then(() => {
+    const server = app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+    server.keepAliveTimeout = 61 * 1000;
+    server.headersTimeout = 122 * 1000;
+  })
+  .catch((err) => {
+    console.log(err);
+  });
